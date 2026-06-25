e139fb5b01020d28f5c49aed336b403c87adb6ae
reddycea <bongabonga434@gmail.com>
2026-06-25 18:19:07 +0200

    Update import paths for backend structure

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,17 +1,20 @@
 import pytest
 
-from rnw import create_app
-from rnw.extensions import db
-from rnw.services.seed_service import seed_database
+from backend.rnw import create_app
+from backend.rnw.extensions import db
+from backend.rnw.services.seed_service import seed_database
 
 
 @pytest.fixture()
 def app():
     app = create_app("testing")
+
     with app.app_context():
         db.create_all()
         seed_database()
+
         yield app
+
         db.session.remove()
         db.drop_all()
 
